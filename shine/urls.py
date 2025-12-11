da069b4e892708c96452944e7d1d31942e365c15
loliette <yelmaniyel@gmail.com>
2025-12-11 13:27:10 +0000

    page,services en place

--- shine/urls.py
+++ shine/urls.py
@@ -14,6 +14,9 @@ urlpatterns = [
     devis_path := path('devis/', shine.views.devis, name='devis'),
     mentorat_path := path('mentorat/', shine.views.mentorat, name='mentorat'),
     infos_bourses_path := path('infos_bourses/', shine.views.infos_bourses, name='infos_bourses'),
+    
+    # SERVICES PAGES
+    etude_path := path('services/etude/', shine.views.etude, name='etude'),
 ] +   static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Serve static and media files during development
